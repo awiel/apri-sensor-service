9c3b86452fc832d723b9d019762c75e35b5b8a60
awiel <awiel@scapeler.com>
2019-01-28 21:51:45 +0100

    time with space into +

--- apri-sensor-service-sensorcontrol.js
+++ apri-sensor-service-sensorcontrol.js
@@ -101,6 +101,9 @@ app.get('/'+_systemCode+'/apri-sensor-service/v1/getCalModelData', function(req,
 	params.ctrlDate = '';
 	if (_query.date != undefined) {
 		params.ctrlDate = _query.date;
+		if (_query.date.substr(19,1)==' ') {   //1899-12-31T00:00:00 00:00  -> 1899-12-31T00:00:00+00:00
+			params.ctrlDate = _query.date.substr(0,18) + '+' + _query.date.substr(20);
+		} else params.ctrlDate = _query.date;
 	}
 	console.log(params.foi);
 
