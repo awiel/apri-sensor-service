da359a5c57298deb1d7f220b1a4b51f49c0955d5
awiel <awiel@scapeler.com>
2019-07-21 00:13:36 +0200

    apri-sensor-service-sensormain

--- apri-sensor-service-ds18b20.sh
+++ apri-sensor-service-ds18b20.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd `dirname $0`
-node apri-sensor-service-ds18b20.js >>$1 2>>$1
+#node apri-sensor-service-ds18b20.js >>$1 2>>$1
+node apri-sensor-service-sensormain.js ds18b20  >>$1 2>>$1
 exit -1
