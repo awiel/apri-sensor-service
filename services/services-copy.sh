cb6cb7ff2de7d4795ac6fef1953f1249d3424c5c
awiel <awiel@scapeler.com>
2024-03-08 23:14:46 +0100

    enable services sh

--- services/services-copy.sh
+++ services/services-copy.sh
@@ -1,23 +1,20 @@
-#cp services/SCAPE604-apri-sensor-service-arduinobin.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-bam1020.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-bme280.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-bme680.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-ds18b20.service.org /etc/systemd/system/.
-#cp services/SCAPE604-apri-sensor-service-dylos.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-ips7100.service.org /etc/systemd/system/.
-#cp services/SCAPE604-apri-sensor-service-knmi.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-nextpm.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-pms7003.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-pmsa003.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-pmsa003nm.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-radiationd.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-scd30.service.org /etc/systemd/system/.
-#cp services/SCAPE604-apri-sensor-service-sensorcontrol.service.org /etc/systemd/system/.
-#cp services/SCAPE604-apri-sensor-service-sensorselect.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-solar.service.org /etc/systemd/system/.
-cp services/SCAPE604-apri-sensor-service-sps30.service.org /etc/systemd/system/.
-#cp services/SCAPE604-apri-sensor-service-tgs5042.service.org /etc/systemd/system/.
-#cp services/SCAPE604-openiod.service.org /etc/systemd/system/.
-
-
-
+#cp services/SCAPE604-apri-sensor-service-arduinobin.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-arduinobin.service
+cp services/SCAPE604-apri-sensor-service-bam1020.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-bam1020.service
+cp services/SCAPE604-apri-sensor-service-bme280.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-bme280.service
+cp services/SCAPE604-apri-sensor-service-bme680.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-bme680.service
+cp services/SCAPE604-apri-sensor-service-ds18b20.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-ds18b20.service
+#cp services/SCAPE604-apri-sensor-service-dylos.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-dylos.service
+cp services/SCAPE604-apri-sensor-service-ips7100.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-ips7100.service
+#cp services/SCAPE604-apri-sensor-service-knmi.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-knmi.service
+cp services/SCAPE604-apri-sensor-service-nextpm.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-nextpm.service
+cp services/SCAPE604-apri-sensor-service-pms7003.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-pms7003.service
+cp services/SCAPE604-apri-sensor-service-pmsa003.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-pmsa003.service
+cp services/SCAPE604-apri-sensor-service-pmsa003nm.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-pmsa003nm.service
+cp services/SCAPE604-apri-sensor-service-radiationd.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-radiationd.service
+cp services/SCAPE604-apri-sensor-service-scd30.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-scd30.service
+#cp services/SCAPE604-apri-sensor-service-sensorcontrol.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-sensorcontrol.service
+#cp services/SCAPE604-apri-sensor-service-sensorselect.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-sensorselect.service
+cp services/SCAPE604-apri-sensor-service-solar.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-solar.service
+cp services/SCAPE604-apri-sensor-service-sps30.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-sps30.service
+#cp services/SCAPE604-apri-sensor-service-tgs5042.service.org /etc/systemd/system/SCAPE604-apri-sensor-service-tgs5042.service
+#cp services/SCAPE604-openiod.service.org /etc/systemd/system/SCAPE604-openiod.service
