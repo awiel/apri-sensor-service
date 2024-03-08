a1b7ef463a10966234175f5ac8e500e3ced3ddc7
awiel <awiel@scapeler.com>
2024-03-08 23:06:28 +0100

    copy & enable services

---
+++ services/services-copy.sh
@@ -0,0 +1,23 @@
+#cp services/SCAPE604-apri-sensor-service-arduinobin.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-bam1020.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-bme280.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-bme680.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-ds18b20.service.org /etc/systemd/system/.
+#cp services/SCAPE604-apri-sensor-service-dylos.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-ips7100.service.org /etc/systemd/system/.
+#cp services/SCAPE604-apri-sensor-service-knmi.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-nextpm.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-pms7003.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-pmsa003.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-pmsa003nm.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-radiationd.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-scd30.service.org /etc/systemd/system/.
+#cp services/SCAPE604-apri-sensor-service-sensorcontrol.service.org /etc/systemd/system/.
+#cp services/SCAPE604-apri-sensor-service-sensorselect.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-solar.service.org /etc/systemd/system/.
+cp services/SCAPE604-apri-sensor-service-sps30.service.org /etc/systemd/system/.
+#cp services/SCAPE604-apri-sensor-service-tgs5042.service.org /etc/systemd/system/.
+#cp services/SCAPE604-openiod.service.org /etc/systemd/system/.
+
+
+
